1eb487bf5ad93002f351fa0423e8931d7976a919
ravi15 <ravikumarduddu1551@gmail.com>
2026-08-14 18:53:13 +0530

    Time: 7 ms (72.45%), Space: 21 MB (48.31%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -2,8 +2,8 @@ class Solution:
     def trap(self, h: List[int]) -> int:
         n = len(h)
         i, j = 0, n-1
-        lm = h[i]
-        rm = h[n-1]
+        lm = h[0]
+        rm = h[-1]
         tot = 0
         while i <= j:
             if lm <= rm:
