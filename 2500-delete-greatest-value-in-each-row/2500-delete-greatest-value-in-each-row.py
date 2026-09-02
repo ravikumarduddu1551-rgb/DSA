bff3f070fb2ba1ee8e130562c86d5c124bc25c94
ravi15 <ravikumarduddu1551@gmail.com>
2026-09-02 17:38:12 +0530

    Time: 11 ms (24.92%), Space: 19.5 MB (16.14%) - LeetHub

---
+++ 2500-delete-greatest-value-in-each-row/2500-delete-greatest-value-in-each-row.py
@@ -0,0 +1,25 @@
+class Solution:
+    def deleteGreatestValue(self, grid: List[List[int]]) -> int:
+        m = 0
+        ma = []
+        ans = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                grid[i][j] = -grid[i][j]
+                
+        for row in grid:
+            heapq.heapify(row)
+
+        for _ in range(len(grid[0])):
+            for row in grid:
+                x = heapq.heappop(row)
+                ma.append(x)
+            ans -= min(ma)
+            ma = []
+        return ans
+
+        
+            
+
+
+        
