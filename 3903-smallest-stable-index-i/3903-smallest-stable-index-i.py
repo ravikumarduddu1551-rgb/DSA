5c937109c08b5470035ea88cf4c19748635aa19a
ravi15 <ravikumarduddu1551@gmail.com>
2026-09-04 15:55:48 +0530

    Time: 7 ms (48.03%), Space: 19.4 MB (31.28%) - LeetHub

--- 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.py
@@ -2,11 +2,12 @@ class Solution:
     def firstStableIndex(self, nums: list[int], k: int) -> int:
         n = len(nums)
         mi = [nums[-1]]*n
-        ma = [nums[0]]*n
-        for i in range(1, n):
-            ma[i] = max(nums[i], ma[i-1])
-            mi[n - 1 - i] = min(nums[n - i - 1], mi[n - i])
+        for i in range(n-2, -1, -1):
+            mi[i] = min(nums[i], mi[i+1])
+        ma = 0
         for i in range(n):
-            if ma[i] - mi[i] <= k:
+            if nums[i] > ma:
+                ma = nums[i]
+            if ma - mi[i] <= k:
                 return i
         return -1
